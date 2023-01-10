e91998d1ffc67a43382636660ce907d6afd33d85
JiangCangLiang <550716970@qq.com>
2023-01-10 17:24:50 +0800

    修复

--- start-xwayland-gnome.sh
+++ start-xwayland-gnome.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 and -s "am broadcast -a AidluxX11Exit -f 0x01000000"  >/dev/null 2>&1 &
 sleep 1
 if [ ! "$(command -v gnome-session)" ]; then
@@ -6,4 +7,4 @@ if [ ! "$(command -v gnome-session)" ]; then
 fi
 and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"  >/dev/null 2>&1 &
 sleep 2
-sudo  env DISPLAY=:1 gnome-session --disable-acceleration-check  >/dev/null 2>&1 &
+echo $1 | sudo -S env DISPLAY=:1 gnome-session --disable-acceleration-check  >/dev/null 2>&1 &
