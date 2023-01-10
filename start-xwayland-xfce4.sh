774edfb7128c3704d84b0a41bda9c656a42a691e
JiangCangLiang <550716970@qq.com>
2023-01-10 17:33:12 +0800

    增加xfce4的启动脚本

---
+++ start-xwayland-xfce4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+and -s "am broadcast -a AidluxX11Exit -f 0x01000000"  >/dev/null 2>&1 &
+sleep 1
+if [ ! "$(command -v xfce4-session)" ]; then
+  echo "Please install  xfce4" >&2
+  exit 1
+fi
+and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"  >/dev/null 2>&1 &
+sleep 2
+echo $1 | sudo -S env DISPLAY=:1 gnome-session --disable-acceleration-check  >/dev/null 2>&1 &
