97c17022b587cac90517e3d7105e345e05f77b37
JiangCangLiang <550716970@qq.com>
2023-01-10 17:36:28 +0800

    修复

--- start-xwayland-gnome.sh
+++ start-xwayland-gnome.sh
@@ -2,7 +2,7 @@
 and -s "am broadcast -a AidluxX11Exit -f 0x01000000"  >/dev/null 2>&1 &
 sleep 1
 if [ ! "$(command -v gnome-session)" ]; then
-  echo "Please install  gnome" >&2
+  echo "please install gnome" >&2
   exit 1
 fi
 and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"  >/dev/null 2>&1 &
--- start-xwayland-xfce4.sh
+++ start-xwayland-xfce4.sh
@@ -2,7 +2,7 @@
 and -s "am broadcast -a AidluxX11Exit -f 0x01000000"  >/dev/null 2>&1 &
 sleep 1
 if [ ! "$(command -v xfce4-session)" ]; then
-  echo "Please install  xfce4" >&2
+  echo "please install xfce4" >&2
   exit 1
 fi
 and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"  >/dev/null 2>&1 &
