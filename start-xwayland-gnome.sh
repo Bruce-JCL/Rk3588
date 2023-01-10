fab87499abe375a1414644ea2ce470012d662e13
JiangCangLiang <550716970@qq.com>
2023-01-10 16:55:35 +0800

    完善

--- start-xwayland-gnome.sh
+++ start-xwayland-gnome.sh
@@ -1,4 +1,9 @@
-if [ ! "$(command -v gnome-session)" ]; then
-  echo "gnome 没有安装" >&2
+and -s "am broadcast -a AidluxX11Exit -f 0x01000000"
+sleep 1
+if [ ! "$(command -v gnomeplea-session)" ]; then
+  echo "Please install  gnome" >&2
   exit 1
 fi
+and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"
+sleep 2
+sudo  env DISPLAY=:1 gnome-session --disable-acceleration-check
