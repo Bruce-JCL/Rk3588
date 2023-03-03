1ef6018b41920b77a4d393a04edcc06f3baaa6ad
root <root@localhost.localdomain>
2023-03-03 10:43:23 +0000

    termux改成aidlux

--- start-xwayland-xfce4.sh
+++ start-xwayland-xfce4.sh
@@ -5,6 +5,6 @@ if [ ! "$(command -v xfce4-session)" ]; then
   echo "please install xfce4" >&2
   exit 1
 fi
-and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"  >/dev/null 2>&1 &
+and -s "su root /data/data/com.aidlux/files/usr/bin/aidlux-x11 :1"  >/dev/null 2>&1 &
 sleep 2
 echo $1 | sudo -S env DISPLAY=:1 xfce4-session  >/dev/null 2>&1 &
