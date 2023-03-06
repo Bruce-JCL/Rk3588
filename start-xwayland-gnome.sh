42ba6e47a5f10e4fc9d46216143d2a38788270b6
root <root@localhost.localdomain>
2023-03-06 03:41:53 +0000

    change gnome

--- start-xwayland-gnome.sh
+++ start-xwayland-gnome.sh
@@ -5,6 +5,6 @@ if [ ! "$(command -v gnome-session)" ]; then
   echo "please install gnome" >&2
   exit 1
 fi
-and -s "su root /data/data/com.aidlux/files/usr/bin/termux-x11 :1"  >/dev/null 2>&1 &
+and -s "su root /data/data/com.aidlux/files/usr/bin/aidlux-x11 :1"  >/dev/null 2>&1 &
 sleep 2
 echo $1 | sudo -S env DISPLAY=:1 gnome-session --disable-acceleration-check  >/dev/null 2>&1 &
